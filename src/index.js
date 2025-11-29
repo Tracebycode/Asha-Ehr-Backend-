a1175cffbd2e78da1cc1fb8d84a35bef624c2afb
Abhishek Barik <abhishekbarik974@gmail.com>
2025-11-29 21:13:46 +0530

    Ready for the production4

--- src/index.js
+++ src/index.js
@@ -24,7 +24,7 @@ app.use("/tasks", require("./routes/task"));
 
 
 
-const PORT = 3000;
+const PORT = process.env.PORT || 4000 
 app.listen(PORT, () => console.log("Server running on port", PORT));
 
 
