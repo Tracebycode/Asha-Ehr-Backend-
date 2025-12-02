22b2ab89bc61a21c5ed671e55cac7ce574eaea44
Abhishek Barik <153587169+Tracebycode@users.noreply.github.com>
2025-12-02 20:16:19 +0000

    sync push and pull

---
+++ src/routes/sync.js
@@ -0,0 +1,9 @@
+const express = require("express");
+const router = express.Router();
+const sync = require("../controllers/syncFamiliesController");
+const auth = require("../middleware/auth");
+
+router.post("/families/push", auth, sync.pushFamilies);
+router.get("/families/pull", auth, sync.pullFamilies);
+
+module.exports = router;
