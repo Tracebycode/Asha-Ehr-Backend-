6a74efd687554dc8ad27b4053e9ff3e431c69cce
Piyush Bhadade <piyushbhadade27@gmail.com>
2025-12-09 06:57:31 +0530

    Add route to get health cases

--- src/routes/phcAdmin.js
+++ src/routes/phcAdmin.js
@@ -1,3 +1,5 @@
+
+
 const express = require("express");
 const router = express.Router();
 
@@ -24,6 +26,12 @@ router.get("/anms", auth, phcOnly, phcAdminController.getAllAnmWorkers);
 
 // ANM FULL DETAILS
 router.get("/anms/:anmId", auth, phcOnly, phcAdminController.getAnmDetails);
+
+router.get(
+  "/cases",
+  auth,
+  phcAdminController.getHealthCases
+);
 
 
 
